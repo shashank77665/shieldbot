e1b76f942d4f5a2716d87992702e4aba2b7e2eb9
shashank <shashank77665@gmail.com>
2025-01-21 20:20:47 +0530

    env removed

--- frontend/lib/keys.dart
+++ frontend/lib/keys.dart
@@ -1,3 +1,5 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-final backend_url = dotenv.env['BACKEND_URL'] ?? 'http://127.0.0.1:5000';
+//final backend_url = dotenv.env['BACKEND_URL'] ?? 'http://127.0.0.1:5000';
+
+final backend_url = 'http://127.0.0.1:5000';
