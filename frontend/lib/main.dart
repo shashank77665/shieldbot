569bff1340f39af9ccbf5875b7c98981843d8c79
shashank <shashank77665@gmail.com>
2025-03-02 13:26:37 +0530

    Auth implemented

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:shieldbot/routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
